5a1554c45d5cce1893c1f505961ddbcb41a69c4b
bhk31 <bhkhandelwal92@gmail.com>
2023-02-03 00:59:54 +0530

    add middleware to specific route

--- myApp.js
+++ myApp.js
@@ -1,28 +1,39 @@
 let express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 
 let app = express();
 console.log("Hello World");
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.use((req, res, next)=> {
-  console.log(req.method + ' ' + req.path + " - " + req.ip)
-  next()
-})
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
 
-app.get("/json", (req, res)=> {  
+app.get("/json", (req, res) => {
   let message = "Hello json";
   let response;
 
-  if(process.env.MESSAGE_STYLE === "uppercase") {
-    response =  message.toUpperCase();  
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = message.toUpperCase();
   } else {
     response = message;
   }
-  
-  res.json({"message": response});
+
+  res.json({ message: response });
 });
+
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
 
 app.use("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
